ad6b7bdcc3f42380af75834a716472360858164e
hiep-ph288 <hiep.ph288@gmail.com>
2022-10-27 14:02:34 +0700

    done

--- src/db/practice.ts
+++ src/db/practice.ts
@@ -10,67 +10,87 @@ export const practiceData: { [key: string]: PracticeData[] } = {
   easy: [
     {
       ques:"Question 1: Which group of animals Tortoises are: ",
-      ans:['','','','','','',''],
+      ans:['R','E','P','T','I','L','E'],
     },
     {
-      ques:"Question : : ",
-      ans:['','','','','','',''],
+      ques:"Question 2: Which is the reptile animal that can not swim?: ",
+      ans:['T','O','R','T','O','I','S','E'],
     },
-    {
-      ques:"Question : : ",
-      ans:['','','','','','',''],
-    },
-    {
-      ques:"Question : : ",
-      ans:['','','','','','',''],
-    },
-    {
-      ques:"Question : : ",
-      ans:['','','','','','',''],
-    },
+    // {
+    //   ques:"Question : : ",
+    //   ans:['','','','','','',''],
+    // },
+    // {
+    //   ques:"Question : : ",
+    //   ans:['','','','','','',''],
+    // },
+    // {
+    //   ques:"Question : : ",
+    //   ans:['','','','','','',''],
+    // },
+    // {
+    //   ques:"Question : : ",
+    //   ans:['','','','','','',''],
+    // },
   ],
   medium: [
     {
-      ques:"Question 1: : ",
-      ans:['','','','','','',''],
+      ques:"Question 1: Which group of animals Tortoises are: ",
+      ans:['R','E','P','T','I','L','E'],
     },
     {
-      ques:"Question : : ",
-      ans:['','','','','','',''],
+      ques:"Question 2: Which is the reptile animal that can not swim?: ",
+      ans:['T','O','R','T','O','I','S','E'],
     },
-    {
-      ques:"Question : : ",
-      ans:['','','','','','',''],
-    },
-    {
-      ques:"Question : : ",
-      ans:['','','','','','',''],
-    },
-    {
-      ques:"Question : : ",
-      ans:['','','','','','',''],
-    },
+    // {
+    //   ques:"Question 1: : ",
+    //   ans:['','','','','','',''],
+    // },
+    // {
+    //   ques:"Question : : ",
+    //   ans:['','','','','','',''],
+    // },
+    // {
+    //   ques:"Question : : ",
+    //   ans:['','','','','','',''],
+    // },
+    // {
+    //   ques:"Question : : ",
+    //   ans:['','','','','','',''],
+    // },
+    // {
+    //   ques:"Question : : ",
+    //   ans:['','','','','','',''],
+    // },
   ],
   hard: [
     {
-      ques:"Question : : ",
-      ans:['','','','','','',''],
+      ques:"Question 1: Which group of animals Tortoises are: ",
+      ans:['R','E','P','T','I','L','E'],
     },
     {
-      ques:"Question : : ",
-      ans:['','','','','','',''],
+      ques:"Question 2: Which is the reptile animal that can not swim?: ",
+      ans:['T','O','R','T','O','I','S','E'],
     },
-    {
-      ques:"Question : : ",
-      ans:['','','','','','',''],
-    },
-    {
-      ques:"Question : : ",
-      ans:['','','','','','',''],
-    },
-    {
-      ques:"Question : : ",
-      ans:['','','','','','',''],
-    },
+    // {
+    //   ques:"Question : : ",
+    //   ans:['','','','','','',''],
+    // },
+    // {
+    //   ques:"Question : : ",
+    //   ans:['','','','','','',''],
+    // },
+    // {
+    //   ques:"Question : : ",
+    //   ans:['','','','','','',''],
+    // },
+    // {
+    //   ques:"Question : : ",
+    //   ans:['','','','','','',''],
+    // },
+    // {
+    //   ques:"Question : : ",
+    //   ans:['','','','','','',''],
+    // },
   ],
 };
--- src/db/quizz.ts
+++ src/db/quizz.ts
@@ -14,6 +14,16 @@ export const quizzData: { [key: string]: QuizzData[] } = {
       choose:['Under the sea','At the place they were born','On the sand'],
       ans: 1
     },
+    {
+      ques:"Question 2: Which group of animals Tortoises are?",
+      choose:['Reptile','Mammal', 'Amphibian'],
+      ans: 0
+    },
+    {
+      ques:"Question 3: Which is the reptile animal that can not swim?",
+      choose:['Turtle','Tortoise','Sulcata Tortoise'],
+      ans: 2
+    },
   ],
   medium: [
     {
@@ -21,12 +31,32 @@ export const quizzData: { [key: string]: QuizzData[] } = {
       choose:['Under the sea','At the place they were born','On the sand'],
       ans: 1
     },
+    {
+      ques:"Question 2: Which group of animals Tortoises are?",
+      choose:['Reptile','Mammal', 'Amphibian'],
+      ans: 0
+    },
+    {
+      ques:"Question 3: Which is the reptile animal that can not swim?",
+      choose:['Turtle','Tortoise','Sulcata Tortoise'],
+      ans: 2
+    },
   ],
   hard: [
     {
       ques:"Question 1: Where do Turtles lay eggs?",
       choose:['Under the sea','At the place they were born','On the sand'],
       ans: 1
+    },
+    {
+      ques:"Question 2: Which group of animals Tortoises are?",
+      choose:['Reptile','Mammal', 'Amphibian'],
+      ans: 0
+    },
+    {
+      ques:"Question 3: Which is the reptile animal that can not swim?",
+      choose:['Turtle','Tortoise','Sulcata Tortoise'],
+      ans: 2
     },
   ],
 };
